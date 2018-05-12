0567ba127cd903a612941827379a0ea02a8884f3
Benjamin Koffel <benjaminkoffel@gmail.com>
2018-05-12 13:00:50 +1000

    use kafka

--- api/init.py
+++
@@ -1,6 +0,0 @@
-import db
-
-execute = db.initialize()
-execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
-execute('DROP TABLE track;')
-execute('CREATE TABLE track (time TIMESTAMP, provider UUID, state TEXT, latitude SMALLINT, longitude SMALLINT);')
